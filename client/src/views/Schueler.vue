f4f35599e1a30ef2790618088dc8f9101f0c4c02
BF_Moritz <43892898+BF-Moritz@users.noreply.github.com>
2021-10-13 00:15:32 +0200

    show schueler + filter

--- client/src/views/Schueler.vue
+++ client/src/views/Schueler.vue
@@ -4,10 +4,16 @@
 			<v-card-text class="px-16 py-10d-flex justify-center mb-6">
 				<v-row>
 					<v-col md="3" offset-md="1">
-						<v-select class="pa-2" :items="klassen" label="Klassen" outlined></v-select>
+						<v-select
+							class="pa-2"
+							:items="klassen"
+							v-model="klassenModel"
+							label="Klassen"
+							outlined
+						></v-select>
 					</v-col>
 					<v-col md="7">
-						<v-text-field class="pa-2" label="Suche" outlined></v-text-field>
+						<v-text-field class="pa-2" v-model="suchModel" label="Suche" outlined></v-text-field>
 					</v-col>
 				</v-row>
 			</v-card-text>
@@ -16,7 +22,7 @@
 		<v-data-table
 			class="mx-10"
 			:headers="schuelerHeader"
-			:items="schueler"
+			:items="visibleSchueler"
 			fixed-header
 			:heigth="200"
 			:disable-pagination="true"
@@ -25,16 +31,16 @@
 			:items-per-page="-1"
 		>
 			<template v-slot:item.einverstaendnis="{ item }">
-				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis[1])">
+				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis1)">
 					Fotos (intern)
 				</v-chip>
-				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis[2])">
+				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis2)">
 					Website
 				</v-chip>
-				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis[3])">
+				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis3)">
 					Tagespresse (extern)
 				</v-chip>
-				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis[4])">
+				<v-chip class="ma-2" :color="chipColor(item.einverstaendnis4)">
 					Video/Tonaufnahmen
 				</v-chip>
 			</template>
@@ -46,11 +52,13 @@
 </template>
 
 <script>
+import { getKlassen } from '../utils/klassen';
 export default {
 	name: 'Schueler',
 	data() {
 		return {
-			klassen: [],
+			klassen: ['Alle Klassen', ...getKlassen()],
+			klassenModel: 'Alle Klassen',
 			schuelerHeader: [
 				{ text: 'Klasse', value: 'klasse' },
 				{ text: 'Nachname', value: 'nachname' },
@@ -58,188 +66,68 @@ export default {
 				{ text: 'Einverständniserklärungen', value: 'einverstaendnis', sortable: false },
 				{ text: 'Actions', value: 'actions', sortable: false }
 			],
-			schueler: [
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: 'hansina',
-					nachname: 'müller',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				},
-				{
-					_id: '614e33288475090540cd6307',
-					vorname: '231z264376126476263471264763247zehjfsgdhjfgsdhjfg231z2',
-					nachname: '231z264376126476263471264763247zehjfsgdhjfgsdhjfg',
-					klasse: 'test',
-					einverstaendnis: {
-						1: undefined,
-						2: false,
-						3: true,
-						4: false
-					}
-				}
-			]
+			schueler: [],
+			visibleSchueler: [],
+			suchModel: ''
 		};
 	},
+
+	async mounted() {
+		try {
+			const response = await fetch('http://localhost:12345/schueler/');
+			if (response.ok) {
+				const schuelerArray = await response.json();
+				console.log(schuelerArray);
+				this.schueler = schuelerArray;
+				this.visibleSchueler = schuelerArray;
+			} else {
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	},
+
 	methods: {
 		show() {},
 		chipColor(value) {
-			if (value === true) {
+			if (value === 'Ja') {
 				return '#32a852';
 			}
-			if (value === false) {
+			if (value === 'Nein') {
 				return '#ab4124';
 			}
 			return '#bd6f26';
+		},
+
+		filterSchueler(klasse, suche) {
+			if (klasse === 'Alle Klassen') {
+				this.visibleSchueler = this.schueler.filter((schueler) => {
+					if (suche === '') return true;
+					if (schueler.vorname.toLowerCase().indexOf(suche.toLowerCase()) >= 0) return true;
+					if (schueler.nachname.toLowerCase().indexOf(suche.toLowerCase()) >= 0) return true;
+					return false;
+				});
+			} else {
+				this.visibleSchueler = this.schueler.filter((schueler) => {
+					if (schueler.klasse === klasse) {
+						if (suche === '') return true;
+						if (schueler.vorname.toLowerCase().indexOf(suche.toLowerCase()) >= 0) return true;
+						if (schueler.nachname.toLowerCase().indexOf(suche.toLowerCase()) >= 0) return true;
+					}
+
+					return false;
+				});
+			}
+		}
+	},
+
+	watch: {
+		klassenModel(value) {
+			this.filterSchueler(value, this.suchModel);
+		},
+
+		suchModel(value) {
+			this.filterSchueler(this.klassenModel, value);
 		}
 	}
 };
